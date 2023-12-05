0754d0203514c60fff80ecc10c87d7f40896f342
bouni <bouni@owee.de>
2023-12-05 08:44:49 +0000

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "sonokai",
+    },
+  },
+  { "sainnhe/sonokai", name = "sonokai", opts = {
+    sonokai_style = "maia",
+  } },
+}
