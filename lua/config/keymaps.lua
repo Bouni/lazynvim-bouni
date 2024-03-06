b8a45e49c93a1009435f9d2d79462221a99b3cdf
Bouni <bouni@owee.de>
2024-03-06 08:15:24 +0100

    Map F7 for toggeling a terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
 
+-- Unset the default nvim q fuctionality
 vim.keymap.set("n", "q", "<Nop>")
+
+-- F7 for starting the terminal
+map("n", "<f7>", function()
+  Util.terminal()
+end, { desc = "Terminal (cwd)" })
