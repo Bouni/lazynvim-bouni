e57457cd574b5faa2d481d48deb7dbdfa4b19b68
Elias Bonauer <elektro@mabi.ch>
2024-04-25 07:46:22 +0200

    Add color highlight plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,4 +1,10 @@
 return {
   { "Matt-Deacalion/vim-systemd-syntax" },
   { "isobit/vim-caddyfile" },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").setup({})
+    end,
+  },
 }
